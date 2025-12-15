c5f51608bb54fc6111eff4a8edf8b071ae2535d6
Raymond-Calitis <b2029raccalitis@pshs.edu.ph>
2025-12-15 22:14:16 +0800

    bio w/ js

---
+++ biography.js
@@ -0,0 +1,19 @@
+// Homepage button
+document.querySelector('.homepagebutton').onclick = function() {
+    window.location.href = 'index.html';
+};
+
+// Merchandise button
+document.querySelector('.merchandisebutton').onclick = function() {
+    window.location.href = 'merchandise.html';
+};
+
+// Stat-lines button
+document.querySelector('.statlinesbutton').onclick = function() {
+    window.location.href = 'stat-lines.html';
+};
+
+// NBA Legacy button
+document.querySelector('.legacybutton').onclick = function() {
+    window.location.href = 'legacy.html';
+};
