29c03f065e54b7e087744a3eaff35bff86549a07
Raymond-Calitis <b2029raccalitis@pshs.edu.ph>
2025-12-15 23:16:28 +0800

    about us not complete

---
+++ about us.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll(".nav-button");
+
+buttons[0].addEventListener("click", () => {
+    window.location.href = "merchandise.html";
+});
+
+buttons[1].addEventListener("click", () => {
+    window.location.href = "biography.html";
+});
+
+buttons[2].addEventListener("click", () => {
+    window.location.href = "stat-lines.html";
+});
+
+buttons[3].addEventListener("click", () => {
+    window.location.href = "legacy.html";
+});
+
+buttons[4].addEventListener("click", () => {
+    window.location.href = "index.html";
+});
