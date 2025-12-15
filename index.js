2bd0d422b47193d7d512e2a24196696131b4ed18
Raymond-Calitis <b2029raccalitis@pshs.edu.ph>
2025-12-15 20:44:29 +0800

    idex w/ js

---
+++ index.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll(".nav-button");
+
+buttons[0].addEventListener("click", () => {
+    window.location.href = "merchandise.html";
+});
+
+buttons[1].addEventListener("click", () => {
+    window.location.href = "biography.html";
+});
+
+buttons[2].addEventListener("click", () => {
+    window.location.href = "stat-lines.html";
+});
+
+buttons[3].addEventListener("click", () => {
+    window.location.href = "legacy.html";
+});
