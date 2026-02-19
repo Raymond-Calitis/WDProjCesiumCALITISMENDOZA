626524d2823c8d0e9783ff3c1a800c760bb5769b
Juan8622 <b2029jmfmendoza@pshs.edu.ph>
2026-02-19 21:20:04 +0800

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,24 @@
+function calculateScore() {
+let score=0;
+let q1 = document.querySelector('input[type="radio"][name="Option"]:checked')
+if (q1 && q1.value  === 'correct') {
+    score++
+}else(score=score)
+let q2 = document.querySelector('input[type="radio"][name="Option1"]:checked')
+if (q2 && q2.value  === 'correct') {
+    score++
+}else(score=score)
+let q3 = document.querySelector('input[type="radio"][name="Option2"]:checked')
+if (q3 && q3.value  === 'correct') {
+    score++
+}else(score=score)
+let q4 = document.querySelector('input[type="radio"][name="Option3"]:checked')
+if (q4 && q4.value  === 'correct') {
+    score++
+}else(score=score)
+let q5 = document.querySelector('input[type="radio"][name="Option4"]:checked')
+if (q5 && q5.value  === 'correct') {
+    score++
+}else(score=score)
+alert("Your score is: " + score + "/5")
+}
